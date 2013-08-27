a066038bfed11bfc82658b35b9dc18fdd039cc05
edbedb <eric.the.brill@hotmail.com>
2013-08-26 19:14:45 -0700

    just a little tweaking

---
+++ public/FirstScreen.js
@@ -0,0 +1,8 @@
+$(function() {
+	$( "#playonescreen" ).click(function() {
+		//window.location = "127.0.0.1:3000";
+		$("html").load("http://127.0.0.1:3000");
+	//	alert( "Handler for playonescreen() called.");
+	});
+});
+	
