a22e5c62cf992436ecf80dde3105895a521a995a
edbedb <eric.the.brill@hotmail.com>
2013-08-27 19:10:51 -0700

    testing localhost port

--- public/FirstScreen.js
+++ public/FirstScreen.js
@@ -3,7 +3,7 @@ $(function() {
 		window.location.href = "http://localhost:3000/onescreen";
 	});
 	$( "#playcomputer" ).click(function() {
-		window.location.href = "http://localhost:3000/computer";
+		window.location.href = "http://127.0.0.1:3000/computer";
 	});
 	$( "#findamatch" ).click(function() {
 		window.location.href = "http://localhost:3000/match";
